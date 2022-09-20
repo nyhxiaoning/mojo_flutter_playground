ca66b3c6fc409e776bc0bb2fca3c4db1b4231dbe
JosephineAkello <josephineakello2030@gmail.com>
2022-09-20 23:42:11 +0200

    adjust code

--- examples/flutter_qr_code/lib/generate_qr_page.dart
+++ examples/flutter_qr_code/lib/generate_qr_page.dart
@@ -10,6 +10,7 @@ class GenerateQRCode extends StatefulWidget {
 
 class GenerateQRCodeState extends State<GenerateQRCode> {
   TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,32 +18,30 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
         title: const Text('Flutter + QR code'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(20),
-                child: TextField(
-                  controller: controller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter your URL'),
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {});
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: ((context) {
-                      return QRImage(controller);
-                    })));
-                  },
-                  child: const Text('GENERATE QR CODE')),
-            ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Enter your URL'),
+            ),
           ),
-        ),
+          ElevatedButton(
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: ((context) {
+                      return QRImage(controller);
+                    }),
+                  ),
+                );
+              },
+              child: const Text('GENERATE QR CODE')),
+        ],
       ),
     );
   }
